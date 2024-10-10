121ff3382979b3bf4f2dcb372b57ce580c737a25
Bram <bramiozo@gmail.com>
2024-10-10 18:20:45 +0200

    working on formatter

--- scripts/finetune_tts.sh
+++ scripts/finetune_tts.sh
@@ -1,4 +1,4 @@
 MODEL_FOLDER=/media/bramiozo/DATA-FAST/TTS/tts_models/gle/tts-vits-cv-ga_seanos_6000
 TTS_FOLDER=/media/bramiozo/Storage1/bramiozo/VIRTUALENVS/Python/seanos-bFLQpzeS-py3.10/lib/python3.10/site-packages/TTS/bin
-CUDA_VISIBLE_DEVICES="0" python $TTS_FOLDER/train_tts.py --config_path $MODEL_FOLDER/config_withPhonemes_reducedSingers_withSpeakerEncoder_fft4098.json --restore_path $MODEL_FOLDER/model_file.pth.tar --grad_accum_steps 1 --start_with_eval true
+CUDA_VISIBLE_DEVICES="0" python $TTS_FOLDER/train_tts.py --config_path $MODEL_FOLDER/config_withPhonemes_reducedSingers_withSpeakerEncoder_fft4098_bestSingers_seperatedByPrep.json --restore_path $MODEL_FOLDER/model_file.pth.tar --grad_accum_steps 1 --start_with_eval true
 
